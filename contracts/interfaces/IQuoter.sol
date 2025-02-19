7a241399b5f6c22538220f8c5aea16142a0bde2b
zakraicik <raicik.zach@gmail.com>
2025-02-18 20:03:04 -0800

    Interfaces for IQuoter, ISwapRouter, and IWETH

--- contracts/interfaces/IQuoter.sol
+++ contracts/interfaces/IQuoter.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.28;
 
 interface IQuoter{
-    
+    function quoteExactInputSingle(
+        address tokenIn,
+        address tokenOut,
+        uint24 fee,
+        uint256 amountIn,
+        uint160 sqrtPriceLimitX96
+    ) external returns (uint256 amountOut);
 }
--- contracts/interfaces/IWETH.sol
+++ contracts/interfaces/IWETH.sol
@@ -2,5 +2,7 @@
 pragma solidity ^0.8.28;
 
 interface IWETH{
+    function deposit() external payable;
     
+    function withdraw(uint256) external;
 }
