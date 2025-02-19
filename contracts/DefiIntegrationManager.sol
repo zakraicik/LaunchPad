aaed4cce1b622d67abd6d6d1103f87e901c4dba5
zakraicik <raicik.zach@gmail.com>
2025-02-19 11:42:35 -0800

    Added function to deposit to yield protocol

--- contracts/DefiIntegrationManager.sol
+++ contracts/DefiIntegrationManager.sol
@@ -14,61 +14,149 @@ contract DefiIntegrationManager is Ownable, ReentrancyGuard  {
     using SafeERC20 for IERC20;
 
     IAavePool public aavePool;
-    ISwapRouter public swapRouter;
-    IQuoter public quoter;
+    ISwapRouter public uniswapRouter;
+    IQuoter public uniswapQuoter;
     TokenRegistry public tokenRegistry;
 
-    constructor(address _owner) Ownable(_owner) {
+    address public campaignFactory;
+    mapping(address => bool) public authorizedCampaigns;
+
+    mapping(address => mapping(address => uint256)) public aaveDeposits;
+
+    uint24 public constant UNISWAP_FEE_TIER = 3000; // 0.3%
+    uint256 public constant SLIPPAGE_TOLERANCE = 50;
+
+    error UnauthorizedAddress();
+    error invalidCampaignFactory(address campaignFactory);
+    error ZeroAmount(uint256 amount);
+    error InsufficientDeposit(address token, uint256 requested, uint256 available);
+    error TokenNotSupported(address token);
+    error SlippageExceeded(uint256 expected, uint256 received);
+    error YieldDepositFailed(string reason);
+    error InvalidAddress();
+
+    event YieldDeposited(address indexed campaign, address indexed token, uint256 amount);
+    event YieldWithdrawn(address indexed campaign, address indexed token, uint256 amount);
+    event YieldHarvested(address indexed campaign, address indexed token, uint256 yieldAmount);
+    event TokenSwapped(address indexed fromToken, address indexed toToken, uint256 amountIn, uint256 amountOut);
+    event CampaignAuthorized(address indexed campaign);
+    event CampaignUnauthorized(address indexed campaign);
+    event ContractFactoryUpdated(address oldAddress, address newAddress);
+    event AavePoolUpdated(address oldAddress, address newAddress);
+    event UniswapRouterUpdated(address oldAddress, address newAddress);
+    event UniswapQuoterUpdated(address oldAddress, address newAddress);
+
+    constructor(
+        address _aavePool, 
+        address _uniswapRouter, 
+        address _uniswapQuoter, 
+        address _tokenRegistry, 
+        address _owner
+    ) Ownable(_owner) {
+
+        if (_aavePool == address(0) || _uniswapRouter == address(0) || 
+            _uniswapQuoter == address(0) || _tokenRegistry == address(0)) {
+            revert InvalidAddress();
+        }
+        aavePool = IAavePool(_aavePool);
+        uniswapRouter = ISwapRouter(_uniswapRouter);
+        uniswapQuoter = IQuoter(_uniswapQuoter);
+        tokenRegistry =  TokenRegistry(_tokenRegistry);
+    }
+
+    modifier onlyCampaignFactory() {
+        if (msg.sender != campaignFactory) {
+            revert invalidCampaignFactory(campaignFactory);
+        }
+        _;
+    }
+
+    modifier onlyCampaign() {
+        if (!authorizedCampaigns[msg.sender]) {
+            revert UnauthorizedAddress();
+        }
+        _;
+    }
+
+    function setCampaignFactory(address _campaignFactory) external onlyOwner {
+        if (_campaignFactory == address(0)){
+            revert InvalidAddress();
+        }
+
+        address oldFactory = campaignFactory;
+        campaignFactory = _campaignFactory;
+
+        emit ContractFactoryUpdated(oldFactory, _campaignFactory);
+    }
+
+    function authorizeCampaign(address _campaign) external onlyCampaignFactory {
+
+        authorizedCampaigns[_campaign] = true;
+
+        emit CampaignAuthorized(_campaign);
 
     }
-    
-    
-    // function depositToYieldProtocol() {
 
-    // }
+    function unauthorizeCampaign(address _campaign) external onlyOwner{
+        authorizedCampaigns[_campaign] = false;
 
-    // function withdrawFromYieldProtocol() {
+        emit CampaignUnauthorized(_campaign);
+    }
 
-    // }
 
-    // function withdrawAllFromYieldProtocol() {
+    function setAavePool(address _aavePool) external onlyOwner{
+        if (_aavePool == address(0)){
+            revert InvalidAddress();
+        }
 
-    // }
+        address oldAavePool = address(aavePool);
+        aavePool = IAavePool(_aavePool);
+        emit AavePoolUpdated(oldAavePool, _aavePool);
+    }
 
-    // function harvestYield() {
+    function setUniswapRouter(address _uniswapRouter) external onlyOwner{
+        if (_uniswapRouter == address(0)){
+            revert InvalidAddress();
+        }
 
-    // }
+        address oldUniswapRouter = address(uniswapRouter);
+        uniswapRouter = ISwapRouter(_uniswapRouter);
+        emit UniswapRouterUpdated(oldUniswapRouter, _uniswapRouter);
 
-    // function swapTokenForTarget() {
+    }
 
-    // }
+    function setUniswapQuoter(address _uniswapQuoter) external onlyOwner {
+        if (_uniswapQuoter == address(0)){
+            revert InvalidAddress();
+        }
 
-    // function wrapETHAndSwapForTarget() {
+        address oldUniswapQuoter = address(uniswapQuoter);
+        uniswapQuoter = IQuoter(_uniswapQuoter);
+        emit UniswapQuoterUpdated(oldUniswapQuoter, _uniswapQuoter);
 
-    // }
+    }
 
-    // function getTargetTokenEquivalent() {
+    function depositToYieldProtocol(address _token, uint256 _amount) external onlyCampaign nonReentrant {
+        if (_amount <= 0) {
+            revert ZeroAmount(_amount);
+        }
 
-    // }
+        if (!tokenRegistry.isTokenSupported(_token)){
+            revert TokenNotSupported(_token);
+        }
 
-    // function getCurrentYieldRate() {
+        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+        IERC20(_token).safeIncreaseAllowance(address(aavePool), _amount);
 
-    // }
+        try aavePool.supply(_token, _amount, address(this), 0) {
+            aaveDeposits[msg.sender][_token] += _amount;
 
-    // function getDepositedAmount() {
-
-    // }
-
-    // function setAavePool() {
-
-    // }
-
-    // function setUniswapRouter() {
-
-    // }
-
-    // function registerCampaign() {
-
-    // }
+            emit YieldDeposited(msg.sender, _token, _amount);
+        } catch Error(string memory reason){
+            revert YieldDepositFailed(reason);
+        } catch {
+            revert YieldDepositFailed("Aave deposit failed");
+        }
+    }
 
 }
