e0b677ad48ffc92ee51b8cd4e8b69e6ccbd225ad
zakraicik <raicik.zach@gmail.com>
2025-02-20 14:04:19 -0800

    Added YieldDistributor

--- contracts/YieldDistributor.sol
+++ contracts/YieldDistributor.sol
@@ -1,2 +1,87 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract YieldDistributor is Ownable {
+
+    address public platformTreasury;
+    uint public platformYieldShare = 2000;
+
+    error InvalidAddress();
+    error InvalidShare(uint256 share);
+    error ShareExceedsMaximum(uint256 share);
+
+    event PlatformTreasuryUpdated(address oldTreasury, address newTreasury);
+    event PlatformYieldShareUpdated(uint256 oldShare, uint256 newShare);
+    event YieldDistributed(
+        address indexed campaign,
+        address indexed token,
+        uint256 totalYield,
+        uint256 creatorShare,
+        uint256 platformShare
+    );
+
+    constructor(address _platformTreasury, address _owner) Ownable(_owner) {
+        if(_platformTreasury == address(0)){
+            revert InvalidAddress();
+        }
+
+        platformTreasury = _platformTreasury;
+    }
+
+    function updatePlatformTreasury(address _platformTreasury) external onlyOwner {
+        if(_platformTreasury == address(0)){
+            revert InvalidAddress();
+        }
+
+        address oldTreasury = platformTreasury;
+        platformTreasury = _platformTreasury;
+
+        emit PlatformTreasuryUpdated(oldTreasury, _platformTreasury);
+    }
+
+    function updatePlatformYieldShare(uint256 _platformYieldShare) external onlyOwner {
+        if(_platformYieldShare > 5000){
+            revert ShareExceedsMaximum(_platformYieldShare);
+        }
+
+        uint256 oldshare = platformYieldShare;
+        platformYieldShare = _platformYieldShare;
+        
+        emit PlatformYieldShareUpdated(oldshare,_platformYieldShare);
+    }
+
+    function calculateYieldShares(uint256 totalYield) 
+        external 
+        view 
+        returns (uint256 creatorShare, uint256 platformShare) {
+        
+        platformShare = (totalYield * platformYieldShare) / 10000;
+        
+        
+        creatorShare = totalYield - platformShare;
+        
+        return (creatorShare, platformShare);
+    }
+
+    function getPlatformTreasury() external view returns (address) {
+        return platformTreasury;
+    }
+
+    function getPlatformYieldShare() external view returns (uint256) {
+        return platformYieldShare;
+    }
+
+    function getYieldSplitPreview(uint256 yieldAmount) 
+        external 
+        view 
+        returns (uint256 creatorAmount, uint256 platformAmount) 
+    {
+        platformAmount = (yieldAmount * platformYieldShare) / 10000;
+        creatorAmount = yieldAmount - platformAmount;
+        
+        return (creatorAmount, platformAmount);
+    }
+    
+}
