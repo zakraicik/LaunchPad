0f10c3a00eb27a8c4ac46f140325696fd2ff8e23
zakraicik <zraicik@corvusinsurance.com>
2025-02-23 08:00:27 -0800

    Added fixture

--- test/TokenRegistry.ts
+++ test/TokenRegistry.ts
@@ -6,6 +6,51 @@ describe('TokenRegistry', function () {
   async function deployTokenRegistryFixture () {
     const [owner, user1, user2] = await ethers.getSigners()
 
-    const MockERC20 = await ethers.getContractFactory('MockERC20')
+    const mockToken1 = await ethers.deployContract('MockERC20', [
+      'Mock Token 1',
+      'MT1',
+      ethers.parseUnits('100')
+    ])
+
+    const mockToken2 = await ethers.deployContract('MockERC20', [
+      'Mock Token 1',
+      'MT1',
+      ethers.parseUnits('100')
+    ])
+
+    const mockWETH = await ethers.deployContract('MockERC20', [
+      'Mock WETH',
+      'METH',
+      ethers.parseUnits('100')
+    ])
+
+    const tokenRegistry = await ethers.deployContract('TokenRegistry', [owner])
+
+    await mockToken1.waitForDeployment()
+    await mockToken1.waitForDeployment()
+    await mockWETH.waitForDeployment()
+    await tokenRegistry.waitForDeployment()
+
+    return {
+      tokenRegistry,
+      mockToken1,
+      mockToken2,
+      mockWETH,
+      owner,
+      user1,
+      user2
+    }
   }
+
+  describe('Deployment', async function () {
+    it('Should deploy all contracts successfully', async function () {
+      const { tokenRegistry, mockToken1, mockToken2, mockWETH, owner } =
+        await loadFixture(deployTokenRegistryFixture)
+
+      expect(await tokenRegistry.getAddress()).to.be.properAddress
+      expect(await mockToken1.getAddress()).to.be.properAddress
+      expect(await mockToken2.getAddress()).to.be.properAddress
+      expect(await mockWETH.getAddress()).to.be.properAddress
+    })
+  })
 })
