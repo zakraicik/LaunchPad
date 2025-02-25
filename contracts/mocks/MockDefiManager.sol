41c7d5dcbde006732a95d2bfdcc68fdfcdb9d1f5
zakraicik <zraicik@corvusinsurance.com>
2025-02-25 13:20:54 -0800

    Added mocks for defiManager and tokenRegistry

--- contracts/mocks/MockDefiManager.sol
+++ contracts/mocks/MockDefiManager.sol
@@ -1,2 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+
+contract MockDefiManager {
+    bool public authorizeSuccess = true;
+    mapping(address => bool) public authorizedCampaigns;
+    
+    function setAuthorizeSuccess(bool success) external {
+        authorizeSuccess = success;
+    }
+    
+    function authorizeCampaign(address campaign) external returns (bool) {
+        if (authorizeSuccess) {
+            authorizedCampaigns[campaign] = true;
+            return true;
+        }
+        return false;
+    }
+    
+    function tokenRegistry() external pure returns (address) {
+        return address(2); // Mock registry address
+    }
+}
--- contracts/mocks/MockTokenRegistry.sol
+++ contracts/mocks/MockTokenRegistry.sol
@@ -1,2 +1,28 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+
+
+contract MockTokenRegistry {
+    mapping(address => bool) public supportedTokens;
+    
+    function addSupportedToken(address token, bool isSupported) external {
+        supportedTokens[token] = isSupported;
+    }
+    
+    function isTokenSupported(address token) external view returns (bool) {
+        return supportedTokens[token];
+    }
+    
+    function getMinContributionAmount(address token) external pure returns (uint256 minimumAmount, uint8 decimals) {
+        return (100, 18);
+    }
+    
+    function getAllSupportedTokens() external view returns(address[] memory) {
+        address[] memory tokens = new address[](1);
+        return tokens;
+    }
+    
+    function getWETH() external pure returns (address) {
+        return address(1); // Mock WETH address
+    }
+}
